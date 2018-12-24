e4256be2df642182a028f40cafdf8925c60bde72
Kateryna Madzhara <kateryna_madzhara@epam.com>
2018-12-24 14:56:30 +0200

    add example test for cucumber

--- CucumberTest/protractor.conf.js
+++ CucumberTest/protractor.conf.js
@@ -2,12 +2,12 @@ exports.config = {
     seleniumAddress: "http://127.0.0.1:4444/wd/hub",
     getPageTimeout: 60000,
     allScriptsTimeout: 500000,  
-    specs: ["features/**/*.feature"],  
-    baseURL: "http://localhost:8080/",  
     framework: "custom",
-    frameworkPath: require.resolve("protractor-cucumber-framework"),
+    specs: ["features/**/*.feature"],  
+    baseURL: "http://localhost:8080/",
+    frameworkPath: "./node_modules/protractor-cucumber-framework",
     cucumberOpts: {
-        require: "../features/step_definitions/*Steps.js",
+        require: ["./features/step_definitions/*.Steps.js"],
         profile: false,
         "no-source": true
       }  
